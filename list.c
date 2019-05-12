5d9345ada02002e29518035bb661b923af9caa5f
Spiridonov Leonid <50545443+GreatoYare@users.noreply.github.com>
2019-05-12 16:16:35 +0300

    fixes

--- list.c
+++ list.c
@@ -3,9 +3,11 @@
 #include "list.h"
 #include "item.h"
 
-List *makeList() {
+List *makeList()
+{
     List *list = malloc(sizeof(List));
-    if (!list) {
+    if (!list)
+    {
         return NULL;
     }
     list->head = NULL;
@@ -13,17 +15,22 @@ List *makeList() {
     return list;
 }
 
-void add(WoWMounts *mount, List *list) {
+void add(WoWMounts *mount, List *list)
+{
     WoWMounts *current = NULL;
     mount->next = NULL;
     mount->id = 0;
 
-    if (list->head == NULL) {
+    if (list->head == NULL)
+    {
         list->head = mount;
-    } else {
+    }
+    else
+    {
         mount->id++;
         current = list->head;
-        while (current->next != NULL) {
+        while (current->next != NULL)
+        {
             mount->id++;
             current = (WoWMounts *) current->next;
         }
@@ -31,46 +38,76 @@ void add(WoWMounts *mount, List *list) {
     }
 }
 
-void addToPreLast(WoWMounts *mount, List *list) {
+void addToPreLast(WoWMounts *mount, List *list)
+{
     WoWMounts *current = NULL;
     WoWMounts *nextMount = NULL;
     mount->next = NULL;
     mount->id = 0;
 
-    if (list->head == NULL) {
+    if (list->head == NULL)
+    {
         list->head = mount;
-    } else {
+    }
+    else
+    {
         mount->id++;
         current = list->head;
 
-        if(current->next == NULL) {
+        if(current->next == NULL)
+        {
             nextMount = (WoWMounts *) current;
         }
-        else {
+        else
+        {
             nextMount = (WoWMounts *) current->next;
         }
 
-        while (nextMount->next != NULL) {
+        while (nextMount->next != NULL)
+        {
             mount->id++;
             current = (WoWMounts *) current->next;
             nextMount = (WoWMounts *) nextMount->next;
         }
 
-        if(nextMount == current) {
+        if(nextMount == current)
+        {
             list->head = mount;
             mount->next = (struct WoWMounts *) current;
         }
-        else {
+        else
+        {
             current->next = (struct WoWMounts *) mount;
             mount->next = (struct WoWMounts *) nextMount;
         }
     }
+    int k = 0;
+    current = list->head;
+    int l = 0;
+    while (current != NULL)
+    {
+        l++;
+        current = (WoWMounts *) current->next;
+    }
+    printf("%d", l);
+    current = list->head;
+    while (current != NULL)
+    {
+        if (k == 1)
+            current->id = l-1;
+        if (current->id == l-2)
+            k = 1;
+        current = (WoWMounts *) current->next;
+    }
 }
 
-WoWMounts *get(int id, List *list) {
+WoWMounts *get(int id, List *list)
+{
     WoWMounts *current = list->head;
-    while (current != NULL) {
-        if (current->id == id) {
+    while (current != NULL)
+    {
+        if (current->id == id)
+        {
             return current;
         }
         current = (WoWMounts *) current->next;
@@ -78,42 +115,54 @@ WoWMounts *get(int id, List *list) {
     return NULL;
 }
 
-int DeletePrevious(int id, List *list) {
+int DeletePrevious(int id, List *list)
+{
     WoWMounts *current = list->head;
     WoWMounts *previous = current;
-    while (current != NULL) {
-        if (current->id == id) {
-            previous->next = current->next;
-            if (current == list->head)
-                list->head = (WoWMounts *) current->next;
-            free(current);
-            return 1;
+    id--;
+    if (id != -1)
+    {
+        while (current != NULL)
+        {
+            if (current->id == id)
+            {
+                previous->next = current->next;
+                if (current == list->head)
+                    list->head = (WoWMounts *) current->next;
+                free(current);
+                return 1;
+            }
+            previous = current;
+            current = (WoWMounts *) current->next;
         }
-        previous = current;
-        current = (WoWMounts *) current->next;
     }
+    else
+        printf("You can't do it, there is no mount with Id = -1, please choose another Id");
     return 0;
 }
 
-void display(List *list) {
+void display(List *list)
+{
     WoWMounts *current = list->head;
-    if (current == NULL) {
+    if (current == NULL)
+    {
         printf("List is empty\n");
         return;
     }
-    int l = 0 ;
-    while (current != NULL) {
+    while (current != NULL)
+    {
         printf("Id: %d\nCost: %d\nName: %s\nQuest: %s\nAchievement: %s\nDrop from: %s\nDrop chance: %lf\nAddition: %s\nLocation: %s\nPercent of holders: %lf\nSubjective rate: %d\n\n",
-               l,current->cost, current->name, current->quest, current->achieve, current->drop, current->chance, current->addon, current->where, current->holders, current->rate);
-        l++;
+               current->id,current->cost, current->name, current->quest, current->achieve, current->drop, current->chance, current->addon, current->where, current->holders, current->rate);
         current = (WoWMounts *) current->next;
     }
 }
 
-void destroy(List *list) {
+void destroy(List *list)
+{
     WoWMounts *current = list->head;
     WoWMounts *next = current;
-    while (current != NULL) {
+    while (current != NULL)
+    {
         next = (WoWMounts *) current->next;
         free(current);
         current = next;
